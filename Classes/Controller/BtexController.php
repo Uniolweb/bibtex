b4cc855a4cdfb7a8a20fd6655dda6aeabf36141b
Sybille Peters <sybille.peters@uni-oldenburg.de>
2017-08-21 16:48:29 +0200

    Removed unused code

--- Classes/Controller/BtexController.php
+++ Classes/Controller/BtexController.php
@@ -17,47 +17,18 @@ class BtexController extends GeneralActionController
         if ($this->settings['template']) $aOlBibtexPars[]='template='.$this->settings['template'];
         if ($this->settings['sortfixed']) $aOlBibtexPars[]='sortfixed='.$this->settings['sortfixed'];
         $sOlBibtexPars = count($aOlBibtexPars)?'&'.implode('&',$aOlBibtexPars):'' ;
-        
-        //mail('volker.burggraef@uni-oldenburg.de','BibTeX-Check',$sOlBibtexPars);
-        
+
         $olBibtexContent = file_get_contents("http://php51.uni-oldenburg.de/www/bib2html_pr/einzel.php?bibtex=".$this->settings['link'].$sOlBibtexPars.$sOlBibtexLang);
-        //$olBibtexContent = file_get_contents("http://www.uni-oldenburg.de/www/bib2html_pr/einzel.php?bibtex=".$this->settings['link'].$sOlBibtexPars.$sOlBibtexLang);
-        
+
         $uriBuilder = clone($this->uriBuilder);
         $url = $uriBuilder->buildFrontendUri();
         $olBibtexContent = str_replace('href="?sort','href="'.$url.'?sort',$olBibtexContent);
         $olBibtexContent = str_replace('<a href="?">','<a href="'.$url.'">',$olBibtexContent);
         if(!$sOlBibtexLang) $olBibtexContent = str_replace('Go to document','Dokument aufrufen',$olBibtexContent);
-        
-//		$this->view->assign('link', $this->settings['link']);
-//		$this->view->assign('key', $this->settings['key']);
-//		$this->view->assign('sort', $this->settings['sort']);
-//		$this->view->assign('allow', $this->settings['allow']);
-//		$this->view->assign('deny', $this->settings['deny']);
-                
-                $this->view->assign('output', $olBibtexContent);
 
-        // initialize the URIBuilder
-        //$uriBuilder = clone $this->uriBuilder;
-        //$uriBuilder->setCreateAbsoluteUri(true);
-        //$uriBuilder->setUseCacheHash(false);
+        $this->view->assign('output', $olBibtexContent);
 
-		// Get parameter:
-		//$view = $_GET['view'];                  // mode because $this->view is already in use.
-		
-		// Parameter aus Backend Konfiguration lesen:
-		// $myValue = $this->settings['id'];
-
-		// Einen link bauen:
-		// $link = $uriBuilder->setArguments(array('meinParameter1' => $meinParameter1))->buildFrontendUri());
-		
-		// Wenn auf eine andere Seite verwiesen werden soll, muss vor dem Aufruf des Uribuilders noch folgende
-		// Zeile eingefügt werden:
-		// $uriBuilder->setTargetPageUid(1); // Page number im Page tree
-		
-        // Etwas dem Template zuweisen:
-        //$this->view->assign('selectedSemester', $semester);
-		
+ 	
 		
 	}
 }
