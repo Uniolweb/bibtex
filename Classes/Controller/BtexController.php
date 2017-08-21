1cb3eee1cff292a1193af35187c4fae017bffc52
Sybille Peters <sybille.peters@uni-oldenburg.de>
2017-08-21 16:57:07 +0200

    Code style

--- Classes/Controller/BtexController.php
+++ Classes/Controller/BtexController.php
@@ -8,27 +8,47 @@ class BtexController extends GeneralActionController
     {
         $sOlBibtexLang = $GLOBALS['TSFE']->tmpl->setup['page.']['config.']['language'] != 'de' ? '&lang=en' : '' ;
         
-        $sOlBibtexSort = isset($_GET['sort'])?$_GET['sort']:$this->settings['sort'];   
+        $sOlBibtexSort = isset($_GET['sort'])?$_GET['sort']:$this->settings['sort'];
         $aOlBibtexPars = array();
-        if ($this->settings['key']) $aOlBibtexPars[]='key='.$this->settings['key'];
-        if ($sOlBibtexSort) $aOlBibtexPars[]='sort='.$sOlBibtexSort;
-        if ($this->settings['allow']) $aOlBibtexPars[]='allow='.$this->settings['allow'];
-        if ($this->settings['deny']) $aOlBibtexPars[]='deny='.$this->settings['deny'];
-        if ($this->settings['template']) $aOlBibtexPars[]='template='.$this->settings['template'];
-        if ($this->settings['sortfixed']) $aOlBibtexPars[]='sortfixed='.$this->settings['sortfixed'];
-        $sOlBibtexPars = count($aOlBibtexPars)?'&'.implode('&',$aOlBibtexPars):'' ;
+        if ($this->settings['key']) {
+            $aOlBibtexPars[] = 'key=' . $this->settings['key'];
+        }
+        if ($sOlBibtexSort) {
+            $aOlBibtexPars[] = 'sort=' . $sOlBibtexSort;
+        }
+        if ($this->settings['allow']) {
+            $aOlBibtexPars[] = 'allow=' . $this->settings['allow'];
+        }
+        if ($this->settings['deny']) {
+            $aOlBibtexPars[] = 'deny=' . $this->settings['deny'];
+        }
+        if ($this->settings['template']) {
+            $aOlBibtexPars[] = 'template=' . $this->settings['template'];
+        }
+        if ($this->settings['sortfixed']) {
+            $aOlBibtexPars[] = 'sortfixed=' . $this->settings['sortfixed'];
+        }
+        $sOlBibtexPars = count($aOlBibtexPars)?'&'.implode('&', $aOlBibtexPars):'' ;
 
-        $olBibtexContent = file_get_contents("http://php51.uni-oldenburg.de/www/bib2html_pr/einzel.php?bibtex=".$this->settings['link'].$sOlBibtexPars.$sOlBibtexLang);
+        $olBibtexContent = file_get_contents("http://php51.uni-oldenburg.de/www/bib2html_pr/einzel.php?bibtex="
+            . $this->settings['link']
+            . $sOlBibtexPars.$sOlBibtexLang);
 
         $uriBuilder = clone($this->uriBuilder);
         $url = $uriBuilder->buildFrontendUri();
-        $olBibtexContent = str_replace('href="?sort','href="'.$url.'?sort',$olBibtexContent);
-        $olBibtexContent = str_replace('<a href="?">','<a href="'.$url.'">',$olBibtexContent);
-        if(!$sOlBibtexLang) $olBibtexContent = str_replace('Go to document','Dokument aufrufen',$olBibtexContent);
+        $olBibtexContent = str_replace('href="?sort', 'href="'.$url.'?sort', $olBibtexContent);
+        $olBibtexContent = str_replace('<a href="?">', '<a href="'.$url.'">', $olBibtexContent);
+        if (!$sOlBibtexLang) {
+            $olBibtexContent = str_replace(
+                'Go to document',
+                'Dokument aufrufen',
+                $olBibtexContent
+            );
+        }
 
         $this->view->assign('output', $olBibtexContent);
 
- 	
-		
-	}
+
+
+    }
 }
--- Classes/Helper/BackendHelper.php
+++ Classes/Helper/BackendHelper.php
@@ -2,39 +2,42 @@
 
 namespace Uniol\Btex\Helper;
 
-class BibtexHelper{
+class BibtexHelper
+{
 
-	function sortOptions($config) {
-		$options = array();
-		
-		// Hier die Optionen hinzufügen
-		$options[] = array("- leer lassen -"," ");
-		
-		$config['items'] = array_merge($options, $config['items']);
+    public function sortOptions($config)
+    {
+        $options = array();
+        
+        // Hier die Optionen hinzufügen
+        $options[] = array("- leer lassen -"," ");
+        
+        $config['items'] = array_merge($options, $config['items']);
 
-		return $config['items'];
-	}
-	
-	function allowOptions($config) {
-		$options = array();
-		
-		// Hier die Optionen hinzufügen
-		$options[] = array("- leer lassen -"," ");
-		
-		$config['items'] = array_merge($options, $config['items']);
+        return $config['items'];
+    }
+    
+    public function allowOptions($config)
+    {
+        $options = array();
+        
+        // Hier die Optionen hinzufügen
+        $options[] = array("- leer lassen -"," ");
+        
+        $config['items'] = array_merge($options, $config['items']);
 
-		return $config['items'];
-	}
-	
-	function denyOptions($config) {
-		$options = array();
-		
-		// Hier die Optionen hinzufügen
-		$options[] = array("- leer lassen -"," ");
-		
-		$config['items'] = array_merge($options, $config['items']);
+        return $config['items'];
+    }
+    
+    public function denyOptions($config)
+    {
+        $options = array();
+        
+        // Hier die Optionen hinzufügen
+        $options[] = array("- leer lassen -"," ");
+        
+        $config['items'] = array_merge($options, $config['items']);
 
-		return $config['items'];
-	}
-	
+        return $config['items'];
+    }
 }
