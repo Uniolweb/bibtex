cf4d018afbb7802c56478a4ea1160a72efc19c18
Sybille Peters <sybille.peters@uni-oldenburg.de>
2020-01-21 16:01:29 +0100

    Refactor

--- Classes/Controller/BtexController.php
+++ Classes/Controller/BtexController.php
@@ -4,8 +4,9 @@ namespace Uniol\Btex\Controller;
 
 use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
-class BtexController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+class BtexController extends ActionController
 {
 
     private $logger;
@@ -59,24 +60,33 @@ class BtexController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
             $olBibtexContent = 'Bibtex file is empty.';
             $this->logger->error('Error on opening URL ' . $bibtexUrl . ': Bibtex file is empty');
         } else {
-            $url = "http://php51.uni-oldenburg.de/www/bib2html_pr/einzel.php?bibtex="
-                . $bibtexUrl
-                . $sOlBibtexPars . $sOlBibtexLang;
+            $urlBibtexService = $this->settings['urlBibtexService'] ?? '';
+            $olBibtexContent = '';
+            if ($urlBibtexService) {
+                //$url = "http://php51.uni-oldenburg.de/www/bib2html_pr/einzel.php?bibtex="
+                $url = $urlBibtexService
+                    . $bibtexUrl
+                    . $sOlBibtexPars . $sOlBibtexLang;
 
-            $olBibtexContent = file_get_contents($url);
-
-            $this->logger->debug("BtexController: url=$url");
-
-            $uriBuilder = clone($this->uriBuilder);
-            $url = $uriBuilder->buildFrontendUri();
-            $olBibtexContent = str_replace('href="?sort', 'href="' . $url . '?sort', $olBibtexContent);
-            $olBibtexContent = str_replace('<a href="?">', '<a href="' . $url . '">', $olBibtexContent);
-            if (!$sOlBibtexLang) {
-                $olBibtexContent = str_replace(
-                    'Go to document',
-                    'Dokument aufrufen',
-                    $olBibtexContent
-                );
+                $this->logger->debug("BtexController: url=$url");
+                $olBibtexContent = file_get_contents($url);
+                if ($olBibtexContent !== false) {
+                    $uriBuilder = clone($this->uriBuilder);
+                    $url = $uriBuilder->buildFrontendUri();
+                    $olBibtexContent = str_replace('href="?sort', 'href="' . $url . '?sort', $olBibtexContent);
+                    $olBibtexContent = str_replace('<a href="?">', '<a href="' . $url . '">', $olBibtexContent);
+                    if (!$sOlBibtexLang) {
+                        $olBibtexContent = str_replace(
+                            'Go to document',
+                            'Dokument aufrufen',
+                            $olBibtexContent
+                        );
+                    }
+                } else {
+                    $this->logger->error('Fetch data from $url returns false');
+                }
+            } else if (!$urlBibtexService) {
+                $this->logger->error('TypoScript setting bibTexUrl is not defined');
             }
         }
 
