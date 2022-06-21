d71652c4e639966addc74c67ba44a4d3577cdcca
Sybille Peters <sybille.peters@uni-oldenburg.de>
2022-06-21 14:51:48 +0200

    [DEPRECATION] Remove vendorname in configurePlugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,42 +3,35 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-$extkey = "bibtex";
-$vendorname = 'Uniolit';
-$extname = "$vendorname.$extkey";
-$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extkey));
+(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Bibtex',
+        'Bibtex',
+        [
+            'Btex' => 'show',
+
+        ]
+    );
+
+    // Page TSconfig
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:bibtex/Configuration/TSconfig/Page/Wizards/NewContentElement.tsconfig'");
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    $extname,
-    'Bibtex',
-    [
-        'Btex' => 'show',
-
-    ],
-    // non-cacheable actions
-    [
-//        'Btex' => 'show'
-    ]
-);
-
-// Page TSconfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:bibtex/Configuration/TSconfig/Page/Wizards/NewContentElement.tsconfig'");
+    // icons
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Imaging\IconRegistry::class
+    );
+    $iconRegistry->registerIcon(
+        'bibtex-plugin', // Icon-Identifier
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:bibtex/Resources/Public/Assets/Icons/BibTeX_logo.svg']
+    );
 
 
-// icons
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Core\Imaging\IconRegistry::class
-);
-$iconRegistry->registerIcon(
-    'bibtex-plugin', // Icon-Identifier
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:bibtex/Resources/Public/Assets/Icons/BibTeX_logo.svg']
-);
+    // caching framework
 
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bibtex_bibtexcache'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bibtex_bibtexcache'] = [];
+    }
 
-// caching framework
-
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bibtex_bibtexcache'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['bibtex_bibtexcache'] = [];
-}
+})();
