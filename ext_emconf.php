84a754e0fdd27b9d209e8e16c8c207ec800729d3
Sybille Peters <sybille.peters@uni-oldenburg.de>
2024-11-07 15:39:40 +0100

    v3.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,23 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = ['title' => 'Bibtex Extension (Mit Uniol Anpassungen)', 'description' => '', 'category' => 'plugin', 'author' => '', 'author_email' => '', 'author_company' => '', 'state' => 'excludeFromUpdates', 'version' => '3.0.0-dev', 'constraints' => [
-    'depends' => [
-        'extbase' => '',
-        'fluid' => '',
-        'typo3' => '11.5.19-11.99.99'
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Bibtex Extension (Mit Uniol Anpassungen)',
+    'description' => '',
+    'category' => 'plugin',
+    'author' => '',
+    'author_email' => '',
+    'author_company' => '',
+    'state' => 'excludeFromUpdates',
+    'version' => '3.0.1',
+    'constraints' => [
+        'depends' => [
+            'extbase' => '',
+            'fluid' => '',
+            'typo3' => '11.5.19-11.99.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
     ],
-    'conflicts' => [],
-    'suggests' => [],
-], 'autoload' => ['classmap' => ['Classes']]];
+    'autoload' => ['classmap' => ['Classes']]
+];
 
