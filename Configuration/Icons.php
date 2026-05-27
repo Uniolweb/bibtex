33dad34f32fa81d5294ba11cb63a4c9de2506663
Sybille Peters <sybille.peters@uni-oldenburg.de>
2026-05-27 14:24:35 +0200

    [TASK] Cleanup icons
    Remove deprecated filename ext_icon.gif, remove all unused icons

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,9 +3,7 @@
 return [
     // icon identifier
     'bibtex-plugin' => [
-        // icon provider class
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        // the source SVG for the SvgIconProvider
-        'source' => 'EXT:bibtex/Resources/Public/Assets/Icons/BibTeX_logo.svg',
+        'source' => 'EXT:bibtex/Resources/Public/Icons/Extension.svg',
     ],
 ];
