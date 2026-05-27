c00e1a973251c13b5c2c93fbefcbd6bfe928128c
Sybille <sybille.peters@uni-oldenburg.de>
2026-05-27 15:16:59 +0200

    [TASK] Remove TSconfig newContentElement wizard (#10)
    Is now configured via TCA (since TYPO3 v13).

--- Configuration/Icons.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    // icon identifier
-    'bibtex-plugin' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bibtex/Resources/Public/Icons/Extension.svg',
-    ],
-];
